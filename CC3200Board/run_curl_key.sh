f54010b5255988fcb83e6265820a1a07130a3c5e
Aroop Biswal <aroopbiswal@gmail.com>
2024-05-20 15:41:46 -0700

    added curl commands to bash scripts

---
+++ CC3200Board/run_curl_key.sh
@@ -0,0 +1,2 @@
+curl --tlsv1.2 "https://a62ofxyy56n2n-ats.iot.us-east-2.amazonaws.com:8443/things/CC3200Board/shadow" --cacert AmazonRootCA1.pem --key 546ae0b9c943b7f953e98a4469013b1eae8371a6239783ba3ed31da6106f48a6-private.pem.key --cert 546ae0b9c943b7f953e98a4469013b1eae8371a6239783ba3ed31da6106f48a6-certificate.pem.crt
+
