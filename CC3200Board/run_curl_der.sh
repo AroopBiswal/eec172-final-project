917cfc65a2d7a2e22608aed4458eba9c57576929
benyoung32 <benyoung32@gmail.com>
2024-06-12 00:57:38 -0700

    remove certs

--- CC3200Board/run_curl_der.sh
+++
@@ -1,6 +0,0 @@
-curl --tlsv1.2 "https://a62ofxyy56n2n-ats.iot.us-east-2.amazonaws.com:8443/things/CC3200Board/shadow" \
-  --cacert AmazonRootCA1.pem \
-  --key private.der \
-  --cert client.der \
-  --cert-type DER --key-type DER
-
